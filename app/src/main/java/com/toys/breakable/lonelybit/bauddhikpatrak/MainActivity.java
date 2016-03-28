b3fbde607cd031b19b64846e297457e89ea016c5
lonelybit <lonelybit12@gmail.com>
2016-03-28 06:46:28 +0530

    Added more layout specific to contents and some sample contents.

--- app/src/main/java/com/toys/breakable/lonelybit/bauddhikpatrak/MainActivity.java
+++ app/src/main/java/com/toys/breakable/lonelybit/bauddhikpatrak/MainActivity.java
@@ -3,6 +3,7 @@ package com.toys.breakable.lonelybit.bauddhikpatrak;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,12 +85,116 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        RelativeLayout mainContentLayout = (RelativeLayout) findViewById(R.id.main_content_layout);
+        switch (id) {
+            case R.id.april_16_amrut :
+                Logger.getLogger("MainActivity").info("Clicked for Amrutvachan in April 2016");
 
+                View amrutview = layoutInflater.inflate(R.layout.parichched_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(amrutview);
+
+                TextView amrutTitle = (TextView)findViewById(R.id.parichched_lable_id);
+                amrutTitle.setText(R.string.amrut_vachan_text);
+                TextView amrutContents = (TextView)findViewById(R.id.parichched_contents);
+                amrutContents.setText(R.string.april_2016_amrut);
+
+
+                break;
+            case R.id.april_16_bodhkatha :
+                View bodhkathaView = layoutInflater.inflate(R.layout.parichched_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(bodhkathaView);
+
+                TextView bothkathaTitle = (TextView)findViewById(R.id.parichched_lable_id);
+                bothkathaTitle.setText(R.string.bodhkatha_text);
+                TextView bodhkathaContents = (TextView)findViewById(R.id.parichched_contents);
+                bodhkathaContents.setText(R.string.april_2016_bodhkatha);
+                break;
+            case R.id.april_16_nirop :
+
+                View niropView = layoutInflater.inflate(R.layout.parichched_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(niropView);
+                break;
+            case R.id.april_16_sanghik :
+
+                View sanghikGeetView = layoutInflater.inflate(R.layout.geet_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(sanghikGeetView);
+                TextView geetTypeTitle = (TextView)findViewById(R.id.geet_type_lable);
+                geetTypeTitle.setText(R.string.sanghik_geet_text);
+
+                TextView geetTitle = (TextView)findViewById(R.id.geet_title);
+                geetTitle.setText(R.string.april_2016_sanghik_geet_title);
+
+                TextView sanghikGeetContents = (TextView)findViewById(R.id.geet_contents);
+                sanghikGeetContents.setText(R.string.april_2016_sanghik_geet);
+                break;
+
+            case R.id.april_16_vaiyaktik :
+
+                View vaiyaktikGeetView = layoutInflater.inflate(R.layout.geet_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(vaiyaktikGeetView);
+
+
+                TextView geetTypeTitle1 = (TextView)findViewById(R.id.geet_type_lable);
+                geetTypeTitle1.setText(R.string.vaiyaktik_geet_text);
+
+                TextView geetTitle1 = (TextView)findViewById(R.id.geet_title);
+                geetTitle1.setText(R.string.april_2016_vaiyaktik_geet_title);
+
+                TextView vGeetContents = (TextView)findViewById(R.id.geet_contents);
+                vGeetContents.setText(R.string.april_2016_vaiyaktik_geet);
+                break;
+            case R.id.april_16_subhashit :
+                View subhashittView = layoutInflater.inflate(R.layout.subhashit_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(subhashittView);
+
+                TextView subhashit = (TextView)findViewById(R.id.subhashit_text_id);
+                subhashit.setText(R.string.april_2016_subhshit);
+
+                TextView subhashitArth = (TextView)findViewById(R.id.subhashit_arth_contents_id);
+                subhashitArth.setText(R.string.april_2016_subhshit_arth);
+                break;
+            case R.id.mukhya_paan :
+
+                View view = layoutInflater.inflate(R.layout.mukhya_paan_layout,
+                        mainContentLayout, false);
+                mainContentLayout.removeAllViewsInLayout();
+                mainContentLayout.addView(view);
+                //TextView dynamicContentsView1 = (TextView) findViewById(R.id.dynamic_contents);
+                //dynamicContentsView1.setText(R.string.mukhya_paan_mahiti);
+                TextView mukyaPaanTitle = (TextView)findViewById(R.id.mukya_paan_title_id);
+                mukyaPaanTitle.setText(R.string.mukhya_paan_mahiti_title);
+
+                TextView mukyaPaanSubTitle = (TextView)findViewById(R.id.mukya_paan_subtitle_id);
+                mukyaPaanSubTitle.setText(R.string.mukhya_paan_mahiti_subtitle);
+
+                TextView mukyaPaanContents = (TextView)findViewById(R.id.mukya_paan_contents_id);
+                mukyaPaanContents.setText(R.string.mukhya_paan_mahiti_contents);
+                break;
+            default:
+
+
+        }
+
+        /*
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else*/
+        if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
